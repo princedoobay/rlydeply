d1416804547ef61d2419dd842e95a046c3d6cdb7
princedoobay <princedoobay17@gmail.com>
2023-03-09 13:30:09 +0530

    rplydeploy

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello World"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True) # change your portnumber to 8080
